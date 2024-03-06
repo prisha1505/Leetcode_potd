723197fe50bf97a59a3a0fb2d05352ba5cd3b372
Priyanshi <97270465+prisha1505@users.noreply.github.com>
2024-03-06 21:28:42 +0530

    problem 147

---
+++ Linked_List_Cycle.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !(head->next))
+            return NULL;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+                break;
+        }
+        
+        if(fast != slow)
+            return NULL;
+        
+        ListNode* entry = head;
+        while(entry != slow) {
+            entry = entry->next;
+            slow  = slow->next;
+        }
+        return entry;
+    }
+};
