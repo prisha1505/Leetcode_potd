704ca85eb80a010d25a3bb9c843e7c11ab40f506
Priyanshi <97270465+prisha1505@users.noreply.github.com>
2024-03-21 21:41:51 +0530

    problem 162

--- Reverse_Linked_List.cpp
+++ Reverse_Linked_List.cpp
@@ -1,47 +1,19 @@
-
 class Solution {
 public:
-     struct Node* mergeTwoLists(Node *list1,Node *list2) {
-        if(list1 == NULL) {
-            return list2;
-        }
-
-        if(list2 == NULL) {
-            return list1;
-        }
-
-        struct Node* result;
-        if(list1->data < list2->data) {
-            result = list1;
-            result->next = mergeTwoLists(list1->next, list2);
-        } else {
-            result = list2;
-            result->next = mergeTwoLists(list1, list2->next);
-        }
-
-        return result;
-    }
-    
-    struct Node* reverseList(struct Node* head) {
-        if(head == NULL || head->next == NULL) {
+     ListNode* reverseSolve(ListNode* head) {
+        
+        if(!head || !head->next)
             return head;
-        }
-
-        struct Node* last = reverseList(head->next); 
-
+        
+        ListNode* last   = reverseSolve(head->next);
         head->next->next = head;
-        head->next = NULL;
-
+        head->next       = NULL;
+            
         return last;
+        
     }
     
-    struct Node * mergeResult(Node *node1,Node *node2) {
-        struct Node* result = mergeTwoLists(node1, node2);
-        
-        
-        result = reverseList(result);
-        
-        return result;
-        
+    ListNode* reverseList(ListNode* head) {
+        return reverseSolve(head);
     }
 };
